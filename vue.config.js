6a3be3b2984508e02cc9a52f9bc437841bb854e0
clover <750363796@qq.com>
2020-09-03 10:56:08 +0800

    假数据初版v0.1

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,9 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/zhcs_bigscreen/'
+    : '/',
   outputDir: 'dist',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
